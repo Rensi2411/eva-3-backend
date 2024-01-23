1a92aa289b4ac0fcd01f551e41bb218e5c1c37c3
Rensi <rensidudhat2411@gmail.com>
2024-01-23 23:31:53 +0530

    done

--- server/server.js
+++ server/server.js
@@ -1,39 +1,10 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
-dotenv.config();
+server.use(middlewares);
+server.use(router);
 
-const app = express();
-
-app.use(express.json());
-
-app.use(cors());
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-const authRoutes = require('./routes/authRoutes');
-const appointmentRoutes = require('./routes/appointmentRoutes');
-
-app.use('/api', authRoutes);
-app.use('/api', appointmentRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the Masai Hospital FullStack App!');
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+server.listen(port);
